808200580c690f889c5fb321dcf2260a57292763
daw <962983053@qq.com>
2022-05-28 22:59:17 +0800

    feat 前端架子

---
+++ front/src/router/index.js
@@ -0,0 +1,18 @@
+import { createWebHashHistory, createRouter } from 'vue-router';
+
+import Home from '@/view/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
